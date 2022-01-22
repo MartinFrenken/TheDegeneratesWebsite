408a61650aa8fa6927adc9dc1479bc66d1e89052
Shuaa Bhatti <shuaabhatti@gmail.com>
2022-01-22 13:05:54 +0100

    Trying something

--- pages/index.js
+++ pages/index.js
@@ -7,36 +7,31 @@ export default function Home() {
   return (
 <div>
 
-      <div
-          style={{
-            backgroundImage: "url('/Naamloos.png')",
-              backgroundPosition: 'center center',
-              backgroundSize: 'cover',
-              backgroundRepeat: 'no-repeat',
-              width:100,
-              height:1900,
+      <div>
+      <img src="/Naamloos.png" alt="BigCo Inc. logo" style={{
+        height:"auto",
+        width:"100%",
+        maxWidth:"100%"
 
-
-
-
-          }}
-      >
-
-
-
+      }}/>
+      
+      
       </div>
+      <Row justify >
+        <Col span={8}>col-8</Col>
+        <Col span={8} style={{top:400}}>
+            col-8
+            <img src="/logo.png"/>
+        </Col>
+        <Col span={8}>col-8</Col>
+      </Row>
 
 
 
-      </div>
+</div>
 
-  )
+
+  );
+
 }
-// <Row justify >
-//     <Col span={8}>col-8</Col>
-//     <Col span={8} style={{top:400}}>
-//         col-8
-//         <img src="/Logo.png"/>
-//     </Col>
-//     <Col span={8}>col-8</Col>
-// </Row>
+      
