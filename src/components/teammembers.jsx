ebd68fd8ac516203e73cb62b486f5479621bd899
Martin Frenken <mfrenken@priogen.com>
2022-02-11 16:39:01 +0100

    Fixed issue with the team

--- src/components/teammembers.jsx
+++ src/components/teammembers.jsx
@@ -8,11 +8,11 @@ export default function TeamMembers(props) {
     }
     return<>
         <Row  type="flex" align="center" justify ="center" >
-    
+
         <Team 
             teamname="Project Manager"
             image={"team/projectmanager.png"}
-            icon={<AiOutlineTwitter style={{fontSize:"39px", color:"white"}}></AiOutlineTwitter>}
+            icon={<AiOutlineTwitter style={{fontSize: "39px", color: "white"}}/>}
             link={"https://twitter.com/The_DegensNFT"}
             intro="Hi, I'm Samuel. I'm responsible for all the business ideas and your passive income. My background is in E-commerce, where I worked in the sales department, sales department in a small ice-making factory and I also had an internship as a financial agent. 
             If you have any questions for me, contact me at sa.sopuch@gmail.com. I will try to reply ASAP">
@@ -21,7 +21,7 @@ export default function TeamMembers(props) {
         <Team 
             teamname="Artist"
             image={"team/artist.png"}
-            icon={<AiOutlineTwitter style={{fontSize:"39px", color:"white"}}></AiOutlineTwitter>}
+            icon={<AiOutlineTwitter style={{fontSize: "39px", color: "white"}}/>}
             link={"https://twitter.com/The_DegensNFT"}
             intro="Hi, I'm Milan and I'm the head of the art part of our project. I'm personally responsible for all the tribes of the DEGENERATES. My background is mainly in photoshop and illustrator, which I did in school. I hope you like the art. My main goal was to stand out from the rest of the competition. 
             If you have any questions for me, contact me at mino.krajniak@gmail.com.
@@ -31,7 +31,7 @@ export default function TeamMembers(props) {
         <Team 
             teamname="Social Manager"
             image={"team/socialmanager.png"}
-            icon={<AiOutlineTwitter style={{fontSize:"39px", color:"white"}}></AiOutlineTwitter>}
+            icon={<AiOutlineTwitter style={{fontSize: "39px", color: "white"}}/>}
             link={"https://twitter.com/The_DegensNFT"}
             intro="Hi, I'm Nina. I'm responsible for your personal well-being on all our platforms. I also help with all sorts of marketing ideas and their transformation into reality. If you need a fast answer to any question, feel free to contact me on Discord where I’m going under the alias TOKI, I will reply to you shortly. You can also write me an email on ninka.dudakova@gmail.com.">
 
@@ -39,13 +39,15 @@ export default function TeamMembers(props) {
         <Team 
             teamname="Designer"
             image={"team/designer.png"}
-            icon={<AiOutlineTwitter style={{fontSize:"39px", color:"white"}}></AiOutlineTwitter>}
+            icon={<AiOutlineTwitter style={{fontSize: "39px", color: "white"}}/>}
             link={"https://twitter.com/The_DegensNFT"}
             intro="Hi, I'm Peter. I'm also responsible for the art, special skins and everything around, but mainly for our social media designs. I'm also really active in the community, so if you need a quick answer just text me on Discord. My alias is IFRUIT. My background is in all kinds of art sorts, My coworkers know me most for my hand sketching and app designing.">
 
         </Team>
-        <p style={{paddingTop:"73px",paddingBottom:"73px",fontSize:"20px", fontWeight:"600",color:"white", wordWrap:"break-word", inlineSize:"1000px", marginRight:"auto",marginLeft:"auto", textAlign:"center"}}>We are a group of crypto and NFT technology enthusiasts, who decided to do one of a kind NFT project together. If you would like to know us more, feel free to join one of our weekly AMA sessions and ask some questions!</p>
-
         </Row>
+            <Row  type="flex" align="center" justify ="center" >
+        <p style={{paddingTop:"73px",paddingBottom:"73px",fontSize:"20px", fontWeight:"600",color:"white", wordWrap:"break-word", inlineSize:"1000px", marginRight:"auto",marginLeft:"auto", textAlign:"center"}}>We are a group of crypto and NFT technology enthusiasts, who decided to do one of a kind NFT project together. If you would like to know us more, feel free to join one of our weekly AMA sessions and ask some questions!</p>
+                </Row>
+
     </>
     }
